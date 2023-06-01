e1ecad437d6e4f750ce1cfef2040d7aee726db60
Sami-Shafi <freelancersami123@gmail.com>
2023-06-01 19:44:35 +0600

    basic tailwind

--- frontend-nextjs/app/page.tsx
+++ frontend-nextjs/app/page.tsx
@@ -4,7 +4,12 @@ export default async function Home() {
 	const projects = await getProjects();
 
 	return (
-		<div>
+		<div className="max-w-5xl mx-auto py-20">
+			<h1 className="text-7xl font-extrabold">
+				Hello I&apos;m
+				<span className="bg-gradient-to-br ">Sami Ib Shafi!</span>
+			</h1>
+
 			{projects.map((project) => (
 				<div key={project._id}>{project.name}</div>
 			))}
